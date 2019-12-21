09dbaddb5a44ffa633c6b8c6b348cbf5b265b87d
Mike Coats <i.am@mikecoats.com>
2019-12-21 19:23:58 +0000

    Add atom feed, fix build script and add weeknotes 004 post

--- _project/main.js
+++ _project/main.js
@@ -1,18 +1,18 @@
 const Metalsmith = require('metalsmith');
 const collections = require('metalsmith-collections');
-const markdown = require('metalsmith-markdown');
-const discoverPartials = require('metalsmith-discover-partials')
-const layouts = require('metalsmith-layouts');
-const permalinks = require('metalsmith-permalinks');
 const metallic = require('metalsmith-metallic');
-const registerHelpers = require('metalsmith-register-helpers');
+const markdown = require('metalsmith-markdown');
 const excerpts = require('metalsmith-excerpts');
+const permalinks = require('metalsmith-permalinks');
+const discoverPartials = require('metalsmith-discover-partials')
+const registerHelpers = require('metalsmith-register-helpers');
+const layouts = require('metalsmith-layouts');
+const atom = require('metalsmith-feed-atom');
 
 Metalsmith(__dirname)
   .metadata({
-    sitename: 'MikeCoats.com',
-    siteurl: 'https://mikecoats.com/',
-    description: 'MikeCoats.com',
+    site:{url: 'https://mikecoats.com/', name:'MikeCoats.com'},
+    description: 'MikeCoats.com'
   })
   .source('./content')
   .destination('..')
@@ -39,6 +39,11 @@ Metalsmith(__dirname)
   }))
   .use(layouts({
     default: 'default.hbs'
+  }))
+  .use(atom({
+    collection: 'posts',
+    limit: false,
+    destination: 'feed/atom/index.xml'
   }))
   .build(function (err) {
     if (err) throw err;
